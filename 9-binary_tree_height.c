70b55c9204adb551776a720c244126e3d18b277b
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-12-01 21:37:58 +0000

    added 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - will calculate height of tree
+ * @tree: tree to measure
+ *
+ * Return: height of binary tree as size_t
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t lh = 0, rh = 0;
