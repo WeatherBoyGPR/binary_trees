e675779d42f9357f40a1a68dffc4705fd8cc74e7
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-12-01 18:31:45 +0000

    updated documentation on 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_parent - tests if binary tree node is child
+ * binary_tree_is_root - tests if binary tree node is root
  * @node: node to test
  *
- * Return: 1 on parent, 0 otherwise and on NULL
+ * Return: 1 on root, 0 otherwise and on NULL
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
