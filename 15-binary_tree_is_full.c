0fb5f19d534dd49dd8948088eee7af1db540f3d9
WeatherBoyGPR <gp.rossijr@gmail.com>
2021-12-02 15:26:57 +0000

    added 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,6 +1,29 @@
 #include "binary_trees.h"
 
 /**
+ * is_full_core - enables recursion in binary_tree_is_full
+ * @tree: tree to check
+ *
+ * Return: 1 on full tree
+ */
+int is_full_core(const binary_tree_t *tree)
+{
+	int res = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->right && tree->left)
+	{
+		res = res & binary_tree_is_full(tree->right);
+		res = res & binary_tree_is_full(tree->left);
+		return (res);
+	}
+	else
+		return (1);
+
+}
+
+/**
  * binary_tree_is_full - will check for incomplete node
  * @tree: tree to check
  *
@@ -8,14 +31,11 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	size_t res = 0;
-
 	if (tree == NULL)
 		return (0);
-	res = res | binary_tree_is_full(tree->right);
-	res = res | binary_tree_is_full(tree->left);
-	if (tree->left || tree->right)
-		if (!(tree->left && tree->right))
-			return (1);
-	return (res);
+	if (tree->right && tree->left)
+		return (is_full_core(tree->left) & is_full_core(tree->right));
+	if (tree->right || tree->right)
+		return (0);
+	return (0);
 }
